b3767c857b5e36709d4dbc51f93f30651b139a99
Matrix Man <132014118+Matrix-Man-MM@users.noreply.github.com>
2024-02-28 03:22:01 -0800

    Start Epsilon Development

--- src/efi.h
+++ src/efi.h
@@ -1,23 +1,9 @@
+#include <stdint.h>
+#include <stdbool.h>
+
 #if __has_include(<uchar.h>)
 #include <uchar.h>
 #endif
-
-#include <stdint.h>
-#include <stddef.h>
-
-#ifndef _UCHAR_H
-typedef uint_least16_t char16_t;
-#endif
-
-typedef uint16_t UINT16;
-typedef uint32_t UINT32;
-typedef uint64_t UINT64;
-typedef uint64_t UINTN;
-typedef char16_t CHAR16;
-typedef void VOID;
-
-typedef UINTN EFI_STATUS;
-typedef VOID* EFI_HANDLE;
 
 #define IN
 #define OUT
@@ -26,66 +12,64 @@ typedef VOID* EFI_HANDLE;
 
 #define EFIAPI __attribute__((ms_abi))
 
-#define EFI_SUCCESS 0
+typedef uint8_t BOOLEAN;
+typedef int64_t INTN;
+typedef uint64_t UINTN;
+typedef int8_t INT8;
+typedef uint8_t UINT8;
+typedef int16_t INT16;
+typedef uint16_t UINT16;
+typedef int32_t INT32;
+typedef uint32_t UINT32;
+typedef int64_t INT64;
+typedef uint64_t UINT64;
+typedef char CHAR8;
 
-typedef struct EFI_SIMPLE_TEXT_INPUT_PROTOCOL EFI_SIMPLE_TEXT_INPUT_PROTOCOL;
+#ifndef _UCHAR_H
+typedef uint_least16_t char16_t;
+#endif
+
+typedef char16_t CHAR16;
+
+typedef void VOID;
 
 typedef struct
 {
-	UINT16 ScanCode;
-	CHAR16 UnicodeChar;
-} EFI_INPUT_KEY;
+	UINT32 TimeLow;
+	UINT16 TimeMid;
+	UINT16 TimeHighAndVersion;
+	UINT8 ClockSeqHighAndReserved;
+	UINT8 ClockSeqLow;
+	UINT8 Node[6];
+} __attribute__ ((packed)) EFI_GUID;
 
-typedef EFI_STATUS (EFIAPI* EFI_INPUT_READ_KEY) (IN EFI_SIMPLE_TEXT_INPUT_PROTOCOL* This, OUT EFI_INPUT_KEY* Key);
+typedef UINTN EFI_STATUS;
+typedef VOID* EFI_HANDLE;
+typedef VOID* EFI_EVENT;
+typedef UINT64 EFI_LBA;
+typedef UINTN EFI_TPL;
 
-typedef struct EFI_SIMPLE_TEXT_INPUT_PROTOCOL
-{
-	void* Reset;
-	EFI_INPUT_READ_KEY ReadKeyStroke;
-	void* WaitForKey;
-} EFI_SIMPLE_TEXT_INPUT_PROTOCOL;
+#define EFI_SUCCESS 0ULL
 
 typedef struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
 
-#define EFI_BLACK 0x00
-#define EFI_BLUE 0x01
-#define EFI_GREEN 0x02
-#define EFI_CYAN 0x03
-#define EFI_RED 0x04
-#define EFI_YELLOW 0x0E
-#define EFI_WHITE 0x0F
-
-#define EFI_TEXT_ATTR(Foreground, Background) ((Foreground) | ((Background) << 4))
-
+typedef EFI_STATUS (EFIAPI* EFI_TEXT_RESET) (IN EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* This, IN BOOLEAN ExtendedVerification);
 typedef EFI_STATUS (EFIAPI* EFI_TEXT_STRING) (IN EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* This, IN CHAR16* String);
-
-typedef EFI_STATUS (EFIAPI* EFI_TEXT_SET_ATTRIBUTE) (IN EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* This, IN UINTN Attribute);
-
 typedef EFI_STATUS (EFIAPI* EFI_TEXT_CLEAR_SCREEN) (IN EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* This);
 
 typedef struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL
 {
-	void* Reset;
+	EFI_TEXT_RESET Reset;
 	EFI_TEXT_STRING OutputString;
 	void* TestString;
 	void* QueryMode;
 	void* SetMode;
-	EFI_TEXT_SET_ATTRIBUTE SetAttribute;
+	void* SetAttribute;
 	EFI_TEXT_CLEAR_SCREEN ClearScreen;
 	void* SetCursorPosition;
 	void* EnableCursor;
 	void* Mode;
 } EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
-
-typedef enum
-{
-	EfiResetCold,
-	EfiResetWarm,
-	EfiResetShutdown,
-	EfiResetPlatformSpecific
-} EFI_RESET_TYPE;
-
-typedef VOID (EFIAPI* EFI_RESET_SYSTEM) (IN EFI_RESET_TYPE ResetType, IN EFI_STATUS ResetStatus, IN UINTN DataSize, IN VOID* ResetData OPTIONAL);
 
 typedef struct
 {
@@ -99,42 +83,18 @@ typedef struct
 typedef struct
 {
 	EFI_TABLE_HEADER Hdr;
-
-	void* GetTime;
-	void* SetTime;
-	void* GetWakeupTime;
-	void* SetWakeupTime;
-
-	void* SetVirtualAddressMap;
-	void* ConvertPointer;
-
-	void* GetVariable;
-	void* GetNextVariableName;
-	void* SetVariable;
-
-	void* GetNextHighMonotonicCount;
-	EFI_RESET_SYSTEM ResetSystem;
-
-	void* UpdateCapsule;
-	void* QueryCapsuleCapabilities;
-
-	void* QueryVariableInfo;
-} EFI_RUNTIME_SERVICES;
-
-typedef struct
-{
-	EFI_TABLE_HEADER Hdr;
-
-	void* FirmwareVendor;
+	CHAR16* FirmwareVendor;
 	UINT32 FirmwareRevision;
-	void* ConsoleInHandle;
-	EFI_SIMPLE_TEXT_INPUT_PROTOCOL* ConIn;
-	void* ConsoleOutHandle;
+	EFI_HANDLE ConsoleInHandle;
+	void* ConIn;
+	EFI_HANDLE ConsoleOutHandle;
 	EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* ConOut;
-	void* StandardErrorHandle;
-	void* StdErr;
-	EFI_RUNTIME_SERVICES* RuntimeServices;
+	EFI_HANDLE StandardErrorHandle;
+	EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* StdErr;
+	void* RuntimeServices;
 	void* BootServices;
 	UINTN NumberOfTableEntries;
 	void* ConfigurationTable;
 } EFI_SYSTEM_TABLE;
+
+typedef EFI_STATUS (EFIAPI* EFI_IMAGE_ENTRY_POINT) (IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE* SystemTable);
