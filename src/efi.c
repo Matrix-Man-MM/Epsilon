bc59b60af5b180f00da041c71c8ad69620f4442d
Matrix Man <132014118+Matrix-Man-MM@users.noreply.github.com>
2024-02-28 03:22:39 -0800

    Start Epsilon Development

--- src/efi.c
+++ src/efi.c
@@ -1,20 +1,15 @@
 #include "efi.h"
 
-EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable)
+EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable)
 {
 	(void)ImageHandle;
+	(void)SystemTable;
 
-	SystemTable->ConOut->SetAttribute(SystemTable->ConOut, EFI_TEXT_ATTR(EFI_WHITE, EFI_BLUE));
+	SystemTable->ConOut->Reset(SystemTable->ConOut, false);
 
 	SystemTable->ConOut->ClearScreen(SystemTable->ConOut);
 
-	SystemTable->ConOut->OutputString(SystemTable->ConOut, u"Welcome to Epsilon!\r\n");
-	SystemTable->ConOut->OutputString(SystemTable->ConOut, u"Press any key to shutdown...");
+	SystemTable->ConOut->OutputString(SystemTable->ConOut, u"EPSILON UEFI OPERATING SYSTEM\r\n");
 
-	EFI_INPUT_KEY key;
-	while (SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn, &key) != EFI_SUCCESS) ;
-
-	SystemTable->RuntimeServices->ResetSystem(EfiResetShutdown, EFI_SUCCESS, 0, NULL);
-
-	return EFI_SUCCESS;
+	while (1) ;
 }
