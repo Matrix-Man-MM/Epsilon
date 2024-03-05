195ea54ef12a5690d8abb89505c6167919c9ca94
Matrix Man <132014118+Matrix-Man-MM@users.noreply.github.com>
2024-03-05 01:45:32 -0800

    Add query mode, set mode, and set attribute implementations to simple text output protocol

--- src/efi.h
+++ src/efi.h
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <stdbool.h>
+#include <stddef.h>
 
 #if __has_include(<uchar.h>)
 #include <uchar.h>
@@ -55,20 +56,63 @@ typedef struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
 
 typedef EFI_STATUS (EFIAPI* EFI_TEXT_RESET) (IN EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* This, IN BOOLEAN ExtendedVerification);
 typedef EFI_STATUS (EFIAPI* EFI_TEXT_STRING) (IN EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* This, IN CHAR16* String);
+typedef EFI_STATUS (EFIAPI* EFI_TEXT_QUERY_MODE) (IN EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* This, IN UINTN ModeNumber, OUT UINTN* Columns, OUT UINTN* Rows);
+typedef EFI_STATUS (EFIAPI* EFI_TEXT_SET_MODE) (IN EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* This, IN UINTN ModeNumber);
+typedef EFI_STATUS (EFIAPI* EFI_TEXT_SET_ATTRIBUTE) (IN EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* This, IN UINTN Attribute);
+
+#define EFI_BLACK 0x00
+#define EFI_BLUE 0x01
+#define EFI_GREEN 0x02
+#define EFI_CYAN 0x03
+#define EFI_RED 0x04
+#define EFI_MAGENTA 0x05
+#define EFI_BROWN 0x06
+#define EFI_LIGHTGRAY 0x07
+#define EFI_BRIGHT 0x08
+#define EFI_DARKGRAY (EFI_BLACK | EFI_BRIGHT) 0x08
+#define EFI_LIGHTBLUE 0x09
+#define EFI_LIGHTGREEN 0x0A
+#define EFI_LIGHTCYAN 0x0B
+#define EFI_LIGHTRED 0x0C
+#define EFI_LIGHTMAGENTA 0x0D
+#define EFI_YELLOW 0x0E
+#define EFI_WHITE 0x0F
+
+#define EFI_BACKGROUND_BLACK 0x00
+#define EFI_BACKGROUND_BLUE 0x10
+#define EFI_BACKGROUND_GREEN 0x20
+#define EFI_BACKGROUND_CYAN 0x30
+#define EFI_BACKGROUND_RED 0x40
+#define EFI_BACKGROUND_MAGENTA 0x50
+#define EFI_BACKGROUND_BROWN 0x60
+#define EFI_BACKGROUND_LIGHTGRAY 0x70
+
+#define EFI_TEXT_ATTR(Foreground, Background) ((Foreground) | ((Background) << 4))
+
 typedef EFI_STATUS (EFIAPI* EFI_TEXT_CLEAR_SCREEN) (IN EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* This);
+
+typedef struct
+{
+	INT32 MaxMode;
+	INT32 Mode;
+	INT32 Attribute;
+	INT32 CursorColumn;
+	INT32 CursorRow;
+	BOOLEAN CursorVisible;
+} SIMPLE_TEXT_OUTPUT_MODE;
 
 typedef struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL
 {
 	EFI_TEXT_RESET Reset;
 	EFI_TEXT_STRING OutputString;
 	void* TestString;
-	void* QueryMode;
-	void* SetMode;
-	void* SetAttribute;
+	EFI_TEXT_QUERY_MODE QueryMode;
+	EFI_TEXT_SET_MODE SetMode;
+	EFI_TEXT_SET_ATTRIBUTE SetAttribute;
 	EFI_TEXT_CLEAR_SCREEN ClearScreen;
 	void* SetCursorPosition;
 	void* EnableCursor;
-	void* Mode;
+	SIMPLE_TEXT_OUTPUT_MODE* Mode;
 } EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
 
 typedef struct
